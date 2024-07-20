5b1345c954581105cdf3541e53665a8fb0a7471c
akarora24 <anil.kum.arora@gmail.com>
2024-07-20 21:19:08 +0530

    BDD Project uploaded
    testing

---
+++ src/test/java/Features/loginfb.feature
@@ -0,0 +1,10 @@
+
+  Feature: to test Login function
+
+    Scenario Outline:
+      Given User should be logged in and should be present on his wall
+      When I type the message in the box
+      And Click on Post Button
+      Then the message should be posted
+      Examples:
+
---
+++ src/test/java/Steps/MyStepdefs.java
@@ -0,0 +1,28 @@
+package Steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Given("User should be logged in and should be present on his wall")
+    public void userShouldBeLoggedInAndShouldBePresentOnHisWall() {
+        System.out.println("Given Executed Successfully");
+    }
+
+    @When("I type the message in the box")
+    public void iTypeTheMessageInTheBox() {
+        System.out.println("When Executed Successfully");
+    }
+
+    @And("Click on Post Button")
+    public void clickOnPostButton() {
+        System.out.println("And Executed Successfully");
+    }
+
+    @Then("the message should be posted")
+    public void theMessageShouldBePosted() {
+        System.out.println("en Executed Successfully");
+    }
+}
