ddc0709fb2437a3c355fb111c83c4c1cad408009
anil4 <anil.kum.arora@gmail.com>
2025-01-28 17:45:19 +0530

    ok

--- src/test/java/Steps/MyStepdefs.java
+++ src/test/java/Steps/MyStepdefs.java
@@ -8,16 +8,20 @@ import io.cucumber.java.en.When;
 public class MyStepdefs {
     @Given("User should be logged in and should be present on his wall")
     public void userShouldBeLoggedInAndShouldBePresentOnHisWall() {
+
         System.out.println("Given Executed Successfully");
     }
 
     @When("I type the message in the box")
     public void iTypeTheMessageInTheBox() {
+
         System.out.println("When Executed Successfully");
+        System.out.println("This is Demo in 2025");
     }
 
     @And("Click on Post Button")
     public void clickOnPostButton() {
+
         System.out.println("And Executed Successfully");
     }
 
